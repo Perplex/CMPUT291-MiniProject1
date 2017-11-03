253519f59782cb9926bfa984c79e513385e99728
Ceegan Hale <ceeganhale@gmail.com>
2017-11-02 18:02:55 -0600

    remove unnessary variables and include exit condition

--- main.py
+++ main.py
@@ -37,7 +37,8 @@ def cust_sign_up(conn, cid, name, address, pwd):
            c1.execute("""INSERT INTO customers VALUES (?, ?, ?, ?);""", (cid, name, address, pwd))
            conn.commit()
            return 1
-        
+
+
 def add_tables(conn):
     c = conn.cursor()
     # note in the below statement, need """ to allow for multiple line script in python"""
@@ -138,76 +139,47 @@ def main():
     c = conn.cursor()
     c.execute('PRAGMA foreign_keys=ON;') # turns on FKs for the DB for the rest of the connection
     conn.commit()
-    username = ""
-    password = ""
-    
+    user_flag = 0
+
     # Following two lines creates and populates data into the database. Remove before submitting (I think. FIXME)
     add_tables(conn)
     test_data(conn)
     
-    print("Welcome!")
+    first_time_resp = input("Welcome! Please choose one of the following options:\n1. Login\n2. Register  \n3. Quit\n\n>")
     
-    first_time_resp = input("Are you a first time user?(y/n)\n")
-    
-    while first_time_resp != 'y' and first_time_resp != 'n':
-        print("\nPlease press y or n")
-        first_time_resp = input("Are you a first time user?(y/n)\n")
+    while first_time_resp != '1' and first_time_resp != '2' and first_time_resp != '3':
+        print(">")
         
-    if first_time_resp == 'n':  # first_time_resp = 'n'
-        resp = input("Would you like to login?(y/n)\n")
-   
-        while resp != 'y' and resp != 'n':
-            print("\nPlease press y or n")
-            resp = input("Would you like to login?(y/n)\n")
-            # removes whitespace from input, may be unneeded since we are using the user to press either y or n:
-            #for char in resp:
-             #   if char != ' ' and char != '\n':
-              #      resp = char
-     
-        if resp == 'y':
-            username = input("Username: ")
-            password = input("Password: ") 
-            while login(conn, username, password) == 0:
-                print("You entered your password or username incorrectly. Please try again.")
-                username = input("Username: ")
-                password = input("Password: ")
-                # assuming first_time_user actually says yes to being first time.   
-        else:
-            print("Exit succesful")
-            return
-            
-    else: # first_time_resp = 'y'
-    
-        new_acc_resp = input("Would you like to make a new account?(y/n)\n")
-    
-        while new_acc_resp != 'y' and new_acc_resp != 'n':
-            print("\nPlease press y or n")
-            new_acc_resp = input("Are you a first time user?(y/n)\n")
-    
-        if new_acc_resp == 'n':
-            print("Exit succesful")
-            return
-        else:
-            # note all agents are assumed to be registered, so there cannot be a new account being made for agents
-            # making new account for customer:
-            print("Please fill out the following required entries to sign up:\n")
-            new_cid = input("Provide a customer ID:")    
-            new_name = input("Enter your full name:")
-            new_addr = input("Enter your address:")
-            new_pwd = input("Enter a password for your account:")
-            
-            while cust_sign_up(conn, new_cid, new_name, new_addr, new_pwd) == 0:
-                
-                print("\nThe given ID is NOT unique.")
-                new_cid = input("Please enter a unique customer ID\n")  
-            
-            print("Success! You have signed up.")
-            
-            # Is it ok if the user is automatically logged in after sign up?
-            password = new_pwd
-            username = new_cid            
-    
-    user_flag = login(conn, username, password)  
+    if first_time_resp == '1':
+        user_flag = login(conn, input("\nUsername: "), input("Password: "))
+        while user_flag == 0:
+            print("You entered your password or username incorrectly. Please try again.")
+            user_flag = login(conn, input("\nUsername: "), input("Password: "))
+
+    elif first_time_resp == '2':
+
+        # note all agents are assumed to be registered, so there cannot be a new account being made for agents
+        # making new account for customer:
+        print("\nPlease fill out the following required entries to sign up\n")
+        new_cid = input("Provide a customer ID: ")
+        new_name = input("Enter your full name: ")
+        new_addr = input("Enter your address: ")
+        new_pwd = input("Enter a password for your account: ")
+
+        while cust_sign_up(conn, new_cid, new_name, new_addr, new_pwd) == 0:
+            print("\nThe given ID is NOT unique.")
+            new_cid = input("Please enter a unique customer ID: \n")
+
+        print("\nSuccess! You have signed up.")
+
+        # Is it ok if the user is automatically logged in after sign up?
+        user_flag = login(conn, new_cid, new_pwd)
+
+    else:
+        conn.close()
+        return
+
+
     # user_flag is the same as security level. That is:
     # if user_flag = 1, user is an agent
     # if user_flag = 2, user is a customer
@@ -221,8 +193,6 @@ def main():
     else:
         print("Welcome to the customer interface")
 
-    
-    conn.commit()    
     conn.close()
 
 if __name__ == '__main__':
